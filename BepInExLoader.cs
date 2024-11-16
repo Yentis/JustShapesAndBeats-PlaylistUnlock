774384c39e81fae0d3df7cf91c5b34283701aecd
Yentis <yentl178@gmail.com>
2024-11-16 12:42:19 +0100

    initial commit

---
+++ BepInExLoader.cs
@@ -0,0 +1,41 @@
+﻿using BepInEx;
+using HarmonyLib;
+
+namespace Trainer
+{
+    [BepInPlugin(GUID, MODNAME, VERSION)]
+    public class BepInExLoader : BepInEx.IL2CPP.BasePlugin
+    {
+        public const string
+            MODNAME = "PlaylistUnlock",
+            AUTHOR = "Yentis",
+            GUID = "com." + AUTHOR + "." + MODNAME,
+            VERSION = "1.0.0";
+
+        public static BepInEx.Logging.ManualLogSource log;
+
+        public BepInExLoader()
+        {
+            log = Log;
+        }
+
+        public override void Load()
+        {
+            log.LogMessage("[PlaylistUnlock] Loading");
+
+            try
+            {
+                var harmony = new Harmony("yentis.playlistunlock.il2cpp");
+
+                harmony.Patch(
+                    typeof(MetaArcadeSongProgress).GetMethod(nameof(MetaArcadeSongProgress.isUnlocked)),
+                    postfix: new HarmonyMethod(typeof(Patches).GetMethod(nameof(Patches.isUnlocked)))
+                );
+            }
+            catch
+            {
+                log.LogError("[PlaylistUnlock] Harmony - FAILED to Apply Patches!");
+            }
+        }
+    }
+}
---
+++ Patches.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEngine;
+
+namespace Trainer
+{
+    public class Patches : MonoBehaviour
+    {
+        public Patches(IntPtr ptr) : base(ptr)
+        {
+            BepInExLoader.log.LogMessage("[PlaylistUnlock] Entered constructor");
+        }
+
+        public static void isUnlocked(ref bool __result)
+        {
+            __result = true;
+        }
+    }
+}
